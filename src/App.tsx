143e90e385317f642b426521d60f13e095cd800b
erobert <etiennerobert33@gmail.com>
2020-11-01 17:25:30 +0100

    improve data retrieval

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 
+interface SearchUser {
+  id: number;
+  name: string;
+  online_status: 'OFFLINE' | 'ONLINE' | 'DATE';
+  is_plus: boolean;
+  picture: {
+    comment: string;
+    url: string;
+  };
+  last_login: string;
+}
+
+interface SearchResult {
+  cursors: {
+    after: string;
+  };
+  items: SearchUser[];
+  total: number;
+}
+
 function App() {
+  const [users, setUsers] = useState<SearchUser[] | null>(null);
+
   useEffect(() => {
     fetch('http://localhost:3000/api/search')
       .then((response) => {
         if (!response.ok) throw new Error('There was an error during search');
         return response.json();
       })
-      .then((data) => console.log(data))
+      .then((data: SearchResult) => setUsers(data.items))
       .catch((error) => console.error(error));
-  });
+  }, []);
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      {users ? `There are ${users.length} loaded users` : 'Loading'}
     </div>
   );
 }
