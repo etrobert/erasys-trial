4658002902e4c97321e8bb91fe6826ed692abd94
erobert <etiennerobert33@gmail.com>
2020-11-01 16:25:36 +0100

    add api call to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/search')
+      .then((response) => {
+        if (!response.ok) throw new Error('There was an error during search');
+        return response.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  });
   return (
     <div className="App">
       <h1>Hello World!</h1>
