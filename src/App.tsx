d0aedee04cecacd9a7707bb11dd2bc10d2fd2628
erobert <etiennerobert33@gmail.com>
2020-11-04 22:59:19 +0100

    add a switch between grid and radar view

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,52 @@ import React, { useState } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 import Grid from './Grid';
+import Radar from './Radar';
 import useUsersLoader from './useUsersLoader';
+
+type View = 'GRID' | 'RADAR';
+enum ChangeViewButtonText {
+  'GRID' = 'Radar',
+  'RADAR' = 'Grid',
+}
 
 function App() {
   const [censored, setCensored] = useState(true);
+  const [view, setView] = useState<View>('GRID');
 
   const { users, loadMoreUsers, status: loaderStatus } = useUsersLoader();
+
+  const renderView = () => {
+    if (!users) return 'Loading...';
+    return view == 'GRID' ? (
+      <Grid
+        users={users}
+        censored={censored}
+        onScrollEnd={loadMoreUsers}
+        loaderStatus={loaderStatus}
+      />
+    ) : (
+      <Radar users={users} censored={censored} online_status="ONLINE" />
+    );
+  };
 
   return (
     <div className="App">
       <button id="censoredButton" onClick={() => setCensored(!censored)}>
         {censored ? 'Uncensored Mode' : 'Censored Mode'}
       </button>
+      <button
+        id="changeViewButton"
+        onClick={() => setView((view) => (view == 'GRID' ? 'RADAR' : 'GRID'))}
+      >
+        {ChangeViewButtonText[view]}
+      </button>
       {loaderStatus == 'ERROR' && (
         <div className="error">
           There has been an error. Try reloading the page.
         </div>
       )}
-      {users ? (
-        <Grid
-          users={users}
-          censored={censored}
-          onScrollEnd={loadMoreUsers}
-          loaderStatus={loaderStatus}
-        />
-      ) : (
-        'Loading'
-      )}
+      {renderView()}
     </div>
   );
 }
