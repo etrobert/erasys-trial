367f6fff84aee6c42e3936e4bc22ffe1594227ae
erobert <etiennerobert33@gmail.com>
2020-11-05 00:44:12 +0100

    add an image when user doesnt have one

--- src/UserCard.tsx
+++ src/UserCard.tsx
@@ -6,6 +6,22 @@ import { statusColor } from './misc';
 import './UserCard.css';
 
 function UserCard(props: { user: User; censored: boolean }) {
+  const noPicture = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+      <circle cx="12" cy="7" r="4"></circle>
+    </svg>
+  );
   return (
     <div className="UserCard">
       {props.user.picture ? (
@@ -15,7 +31,7 @@ function UserCard(props: { user: User; censored: boolean }) {
           censored={props.censored}
         />
       ) : (
-        'no picture'
+        noPicture
       )}
       <span className="name">{props.user.name}</span>
       <div
