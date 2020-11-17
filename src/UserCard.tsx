9b51480b3dfba7979e549e06f2280a68220a0d8f
erobert <etiennerobert33@gmail.com>
2020-11-17 17:43:28 +0100

    make card text scale based on card size

--- src/UserCard.tsx
+++ src/UserCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { hot } from 'react-hot-loader';
 import { User } from './api';
 import ProfilePhoto from './ProfilePhoto';
@@ -14,10 +14,27 @@ function UserCard(props: {
   onClick?: () => void;
   focused?: boolean;
 }) {
+  const cardRef = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState<number>();
+
+  const ratio = 13.5; // font-size = (element-width / ratio) px;
+
+  useEffect(() => {
+    const card = cardRef.current!;
+    const observer = new ResizeObserver((entries) => {
+      const cardEntry = entries[0];
+      setWidth(cardEntry.contentRect.width);
+    });
+    observer.observe(card);
+    return () => observer.unobserve(card);
+  }, []);
+
   return (
     <div
       className={'UserCard' + (props.focused ? ' focused' : '')}
       onClick={props.onClick}
+      ref={cardRef}
+      style={width ? { fontSize: width / ratio } : undefined}
     >
       <ProfilePhoto picture={props.user.picture} censored={props.censored} />
       <div className="slider">
